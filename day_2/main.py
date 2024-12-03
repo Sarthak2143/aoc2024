9e4403644e022d7db4479b43dc7ce9f75228c032
Sarthak Tomar <sarthaktomar2143@gmail.com>
2024-12-03 22:46:37 +0530

    add part 2 for day 2

--- day_2/main.py
+++ day_2/main.py
@@ -4,8 +4,7 @@ with open("input.txt", "r") as f:
 for i in range(len(data)):
     data[i] = [int(j) for j in data[i].split(" ")]
 
-safe = 0
-for row in data:
+def check_row_safety(row):
     status = False
     if row == sorted(row) or row == sorted(row, reverse=True):
         for i in range(len(row)-1):
@@ -14,6 +13,26 @@ for row in data:
             else:
                 status = False
                 break
-    if status == True: safe += 1
+    return status
 
-print(safe)
+def part_1():
+    safe = 0
+    for row in data:
+        if check_row_safety(row) == True: safe += 1
+
+    print(safe)
+
+def part_2():
+    safe = 0
+    for row in data:
+        if check_row_safety(row) == True: safe += 1
+        else:
+            for i in range(len(row)):
+                new_list = row[:i] + row[i+1:]
+                if check_row_safety(new_list):
+                    safe += 1
+                    break
+    
+    print(safe)
+
+part_2()
